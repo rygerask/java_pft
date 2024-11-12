abd70053fff9ec23e93862b26ddb7ba6181cb4a7
Ваше Имя <you@example.com>
2024-11-12 16:14:48 +0300

    classes

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,15 +5,25 @@ public class MyFirstProgram {
 	public static void main (String[] args) {
 		hello("world");
 		hello("Varvara");
-		System.out.println("She is " + age() + ".");
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(5, 3);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " +
+				r.b + " = " + area(r));
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello," + somebody + "!");
 	}
 
-	public static int age() {
-		int age = 28;
-		return age;
+	public static double area(Square s){
+		return s.l * s.l;
 	}
+
+	public static double area(Rectangle r){
+		return r.a * r.b;
+	}
+
 }
