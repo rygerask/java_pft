73dbb39ed41211316378a82989493d7a910bd253
Ваше Имя <you@example.com>
2024-11-12 15:53:54 +0300

    new functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args) {
-		System.out.println("Hello, world!");
+		hello("world");
+		hello("Varvara");
+		System.out.println("She is " + age() + ".");
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello," + somebody + "!");
+	}
+
+	public static int age() {
+		int age = 28;
+		return age;
 	}
 }
